1af4c33a921a984b65aa5699f046132fc3ba58b6
Nilson Barboza <barbosa.nilsonneto@gmail.com>
2025-04-03 10:52:51 -0300

    implementar sql

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def cadastro(request):
- if request.method == 'GET':
-    return render(request, 'cadastro.html')
+   if request.method == 'GET':
+      return render(request, 'cadastro.html')
+   else:
+      # Valores pegos do meu forms
+      username = request.POST.get('username')
+      senha = request.POST.get('senha')
+      confirmar_senha = request.POST.get('confirmar_senha')
+      
+      if not senha == confirmar_senha:
+         return redirect('/usuarios/cadastro')
+
+      if len(senha) < 6:
+         return redirect('/usuarios/cadastro')
+   
+      users = User.objects.filter(username=username)
+      if users.exists():
+         return redirect('/usuarios/cadastro')
+
+      User.objects.create_user(
+      username=username,
+      password=senha
+      )
+
+   return redirect('/usuarios/cadastro')
