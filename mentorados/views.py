0ceee8dbde9257153b24c180e79ba501d4d50f1b
Nilson Barboza <barbosa.nilsonneto@gmail.com>
2025-04-06 16:38:56 -0300

    Mentorados finalizado

--- mentorados/views.py
+++ mentorados/views.py
@@ -7,11 +7,25 @@ from django.contrib.messages import constants
 def mentorados(request):
     if not request.user.is_authenticated:
         return redirect('login')
+    
     if request.method == 'GET':
         navigators = Navigators.objects.filter(user=request.user)
         mentorados = Mentorados.objects.filter(user=request.user)
         print(mentorados)
-        return render(request, 'mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators, 'mentorados': mentorados})
+
+        #qtd_estagios = []
+        #estagios_flat = [i[1] for i in Mentorados.estagio_choices]
+        #x = Mentorados.objects.filter(estagio=1).filter(user=request.user).count()
+        #qtd_estagios.append(x)
+
+        estagios_flat = [i[1] for i in Mentorados.estagio_choices]
+        qtd_estagios = []
+
+        for i, j in Mentorados.estagio_choices:
+            qtd_estagios.append(Mentorados.objects.filter(estagio=i).count())
+
+
+        return render(request, 'mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators, 'mentorados': mentorados,'estagios_flat': estagios_flat, 'qtd_estagios': qtd_estagios})
     elif request.method== 'POST':
         nome = request.POST.get('nome')
         foto = request.FILES.get('foto')
